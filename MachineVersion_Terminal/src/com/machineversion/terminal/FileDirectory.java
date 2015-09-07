eb7c091ab7aad7e354ffdd54d962ce0766921449
hust-MC <251887584@qq.com>
2015-09-07 15:43:39 +0800

    command

--- MachineVersion_Terminal/src/com/machineversion/terminal/FileDirectory.java
+++ MachineVersion_Terminal/src/com/machineversion/terminal/FileDirectory.java
@@ -7,6 +7,11 @@ public class FileDirectory
 	public static final String appDirectory = Environment
 			.getExternalStorageDirectory() + "/MachineVision/";
 
+	/**
+	 * 获取App的目录路径
+	 * 
+	 * @return app根目录
+	 */
 	public static String getAppDirectory()
 	{
 		return appDirectory;
